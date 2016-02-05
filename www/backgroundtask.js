6f682220752baf4298918550e72de78fb87c26cb
Sean Langley <sean@Tantive-IV.local>
2016-02-05 16:31:10 -0500

    adding parameter to end call

--- www/backgroundtask.js
+++ www/backgroundtask.js
@@ -9,8 +9,8 @@ BackgroundTask.prototype.start = function (task) {
     exec(task, null, "BackgroundTask", "start", []);
 };
 
-BackgroundTask.prototype.end = function () {
-    exec(null, null, "BackgroundTask", "end", []);
+BackgroundTask.prototype.end = function (task) {
+    exec(task, null, "BackgroundTask", "end", []);
 };
 
 module.exports = new BackgroundTask();
